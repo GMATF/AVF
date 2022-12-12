192a30f9a7bcb7189317f70f5cbc171f9172bc3b
Viren Keswani <virenkeswani@gmail.com>
2022-12-12 12:34:56 +0530

    basic prototype

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask ( __name__ )
 def index():
     return render_template('index.html')
 
+@app.route('/contact' , methods=['GET'])
+def contact():
+    return render_template('contact.html')
+
+
 
 
 
